8803a5a12d7fb87fb63957f737990b3922e3f2e1
Nick Budak <thatbudakguy@gmail.com>
2019-01-13 21:04:29 -0500

    remove old tests, add fake test

---
+++ tests/test_util.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import patch
+from subprocess import run
+
+import pytest
+
+from dphon.util import lookup
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_lookup(self):
+        return True

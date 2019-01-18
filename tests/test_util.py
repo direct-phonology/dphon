9062863e7bd385f2e4f4242916dba48b724eaaff
Nick Budak <thatbudakguy@gmail.com>
2019-01-18 13:20:38 -0500

    set up travis CI for testing (#4)
    * basic travis setup
    
    * pin pytest>=3.6 for pytest-cov
    
    * remove pythons 3<x<3.4 (pre-2014
    
    * update supported python version classifiers
    
    * run on xenial for py3.7
    
    * remove py2 compatibility due to type annotations

---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch
+
+import pytest
+
+from dphon.util import lookup
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_lookup(self):
+        return True

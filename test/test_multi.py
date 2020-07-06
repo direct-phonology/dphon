c1b929e674feccd7d18d0b583e883ec78d324ce4
Nick Budak <nbudak@princeton.edu>
2020-07-06 11:45:09 -0400

    split out matching/aligning

--- test/test_multi.py
+++ test/test_multi.py
@@ -1,3 +1,5 @@
+import time
+
 from dphon.loader import KanripoLoader, SimpleLoader
 from dphon.tokenizer import NgramTokenizer
 from dphon.filter import NonAlphaFilter, PhoneticFilter
@@ -12,38 +14,44 @@ schuessler = PhoneticFilter("data/dummy_dict.json")
 direct = LevenshteinPhoneticMatcher("data/dummy_dict.json", threshold=0.75, limit=50)
 nw_phon = NeedlemanWunschPhonetic("data/dummy_dict.json")
 
+# global timer
+start_time = time.time()
+
 # load corpus
 corpus = KanripoLoader("./test/fixtures/krp/", clean=True)
 # corpus = SimpleLoader("./test/fixtures/")
+load_time = time.time() - start_time
+print(f"Loaded corpus in {load_time:.2f} seconds")
 
 # index phonetic content of all documents
 for doc in corpus.docs():
-    index.add(
-        schuessler.process(
-            NonAlphaFilter.process(
-                quadgrams.tokenize(doc)
-            )
-        )
-    )
+    index.add(schuessler.process(quadgrams.tokenize(doc)))
+index_time = time.time() - start_time - load_time
+print(f"Indexed documents in {index_time:.2f} seconds")
 
 # keep only tokens that occur in at least two places
 index.drop(lambda tokens: len(tokens) < 1)
 
-# for testing: keep only groups where exactly two texts match
+# FIXME testing: keep only groups where exactly two different docs match 
 index.drop(lambda tokens: len(tokens) != 2)
-
-# keep only groups with graphic variation
-# FIXME this should be done _after_ extension since otherwise we would need to
-# extend both directions
-# index.drop(lambda tokens: tokens[0].meta["orig_text"] == tokens[1].meta["orig_text"])
-
-# discard documents that match themselves
 index.drop(lambda tokens: tokens[0].doc.title == tokens[1].doc.title)
+prune_time = time.time() - start_time - load_time - index_time
+print(f"Pruned index in {prune_time:.2f} seconds")
 
+# extend and align seeds
+matches = []
 for (seed, tokens) in index.tokens():
+    # only work on matches with graphic variation
     if tokens[0].meta["orig_text"] != tokens[1].meta["orig_text"]:
-        match = direct.extend(tokens[0], tokens[1])
-        (q1, q2) = nw_phon.align(match)
-        print(f"{q1} ({match.source.doc.title}:{match.source.doc.meta['JUAN']})\n{q2} ({match.target.doc.title}:{match.target.doc.meta['JUAN']})\n")
-    # print("%.02f" % match.meta["score"])
-    # print(match)
+        matches.append(direct.extend(tokens[0], tokens[1]))
+match_time = time.time() - start_time - load_time - index_time - prune_time
+print(f"Extended seeds in {match_time:.2f} seconds")
+
+aligned_matches = [nw_phon.align(match) for match in matches]
+align_time = time.time() - start_time - load_time - index_time - prune_time - match_time
+print(f"Aligned matches in {align_time:.2f} seconds")
+
+# for i in range(len(matches)):
+#     match = matches[i]
+#     (q1, q2) = aligned_matches[i]
+#     print(f"{q1} ({match.source.doc.title}:{match.source.doc.meta['JUAN']})\n{q2} ({match.target.doc.title}:{match.target.doc.meta['JUAN']})\n")

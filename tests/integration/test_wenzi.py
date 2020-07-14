23ed461cb00a85b6945eb8ae4824719f9abe047b
Nick Budak <nbudak@princeton.edu>
2020-07-14 10:08:17 -0400

    move tests into folders by type

---
+++ tests/integration/test_wenzi.py
@@ -0,0 +1,7 @@
+"""
+These tests compare the results of DIRECT against identified textual parallels
+that have already been published. The source material is "The Wenzi with
+Parallel Passages from Other Pre-Han and Han Texts" (《文子》與先秦兩漢典籍重見資料彙編).
+"""
+
+

8db19ad88412f099b6ec92bae79a1ed84877edaa
Nick Budak <nbudak@princeton.edu>
2020-08-30 11:55:54 -0400

    make index more generic

--- dphon/index.py
+++ dphon/index.py
@@ -2,7 +2,7 @@
 
 import logging
 from itertools import dropwhile
-from typing import Any, Callable, Iterator, List, Tuple, Generic, TypeVar
+from typing import Any, Callable, Iterator, Iterable, List, Tuple, TypeVar
 
 from spacy.language import Language
 from spacy.lookups import Table
@@ -12,80 +12,57 @@ from spacy.tokens import Doc, Span, Token
 # https://spacy.io/usage/processing-pipelines#custom-components-factories
 Language.factories["index"] = lambda nlp, **cfg: Index(nlp, **cfg)
 
+KT = TypeVar("KT")              # type for keys stored in the index
+VT = TypeVar("VT")              # type for values stored in the index
+Entry = Tuple[KT, List[VT]]     # type for an index entry (all values at key)
+
+
 class Index():
-    """A spaCy pipeline component that indexes Tokens via a callable."""
+    """A spaCy pipeline component that indexes arbitrary items via callables."""
 
-    name: str  # component name, will show up in the pipeline as "[name]_index"
-
-    def __init__(self, nlp: Language, name: str, fn: Callable[[Token], Any]):
+    def __init__(self, nlp: Language, name: str, key_fn: Callable[[KT], VT],
+                 val_fn: Callable[[Doc], Iterable[KT]]):
         """Initialize the index component."""
-        logging.info(f"initializing index pipeline component")
-
-        # store name, callable used to index tokens, and total token count
-        self.fn = fn
-        self.name = f"{name}_index"
-        self._token_count = 0
+        # store name, callables used to index values, and total values (size)
+        self.name = name
+        self.key_fn = key_fn
+        self.val_fn = val_fn
+        self._size = 0
 
         # initialize the index
         self.table = nlp.vocab.lookups.add_table(self.name)
-        logging.info(f"index added to vocab as lookups.{self.name}")
+        logging.info(f"created {self.__class__} as {self.name}")
 
     def __call__(self, doc: Doc) -> Doc:
-        """Index each Token in a Doc at a key returned by calling a function."""
-        for token in doc:
-            key = self.fn(token)
+        """Extract useful values from the Doc and add them to the index."""
+        for val in self.val_fn(doc):
+            key = self.key_fn(val)
+            # if key_fn returns None, skip indexing this value
             if key is not None:
-                if key not in self.table:
-                    self.table[key] = []
-                self.table[key].append(token)
-                self._token_count += 1
+                try:
+                    self.table[key].append(val)
+                except KeyError:
+                    self.table[key] = [val]
+                self._size += 1
         return doc
 
     def __len__(self) -> int:
-        """Get the total number of entries in the index."""
+        """Get the total number of keys in the index."""
         return len(self.table)
 
-    def __getitem__(self, key: Any) -> List[Token]:
-        """Return a list of all Tokens indexed at a given key."""
+    def __getitem__(self, key: KT) -> List[VT]:
+        """Return a list of all values indexed at a given key."""
         return self.table[key]
 
-    def __iter__(self) -> Iterator[Tuple[Any, List[Token]]]:
+    def __iter__(self) -> Iterator[Entry]:
         """Return a (k, v) iterator over all entries in the index."""
-        return ((key, tokens) for key, tokens in self.table.items())
+        return ((key, values) for key, values in self.table.items())
 
-    def filter(self, fn: Callable[[Tuple[Any, List[Token]]], bool]) -> Iterator[Tuple[Any, List[Token]]]:
+    def filter(self, fn: Callable[[Entry], bool]) -> Iterator[Entry]:
         """Return a (k, v) iterator over all entries which match a predicate."""
-        return ((key, tokens) for key, tokens in dropwhile(fn, self.table.items()))
+        return ((key, values) for key, values in dropwhile(fn, self.table.items()))
 
     @property
-    def token_count(self) -> int:
-        """Get the total number of tokens in the index."""
-        return self._token_count
-
-
-class NgramIndex(Index):
-    """A spaCy pipeline component that indexes Token n-grams via a callable."""
-
-    def __init__(self, nlp: Language, name: str, fn: Callable[[Span], Any]):
-        """Initialize the n-gram index component."""
-        logging.info(f"initializing n-gram index pipeline component")
-
-        # store name, callable used to index tokens, and total token count
-        self.fn = fn
-        self.name = f"{name}_ngram_index"
-        self._token_count = 0
-
-        # initialize the index
-        self.table = nlp.vocab.lookups.add_table(self.name)
-        logging.info(f"index added to vocab as lookups.{self.name}")
-
-    def __call__(self, doc: Doc) -> Doc:
-        """Index each n-gram in a Doc at a key returned by calling a function."""
-        for ngram in doc:
-            key = self.fn(ngram)
-            if key is not None:
-                if key not in self.table:
-                    self.table[key] = []
-                self.table[key].append(ngram)
-                self._token_count += 1
-        return doc
+    def size(self) -> int:
+        """Get the total number of values in the index."""
+        return self._size

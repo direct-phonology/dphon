ae83051fb8b9721a6e4f644fb94cd7b3bcebc71a
Nick Budak <nbudak@princeton.edu>
2020-09-04 15:09:36 -0400

    make Ngrams handle empty doc

--- dphon/ngrams.py
+++ dphon/ngrams.py
@@ -14,18 +14,27 @@ Language.factories["ngrams"] = lambda nlp, **cfg: Ngrams(nlp, **cfg)
 class Ngrams():
     """A spaCy pipeline component for generating Token n-grams from Docs."""
 
-    def __init__(self, nlp: Language, name: str, n: int, attr: str = "ngrams"):
+    name = "ngrams"  # will appear in spaCy pipeline
+    attr = "ngrams"  # name for getter for ngrams, e.g. doc._.ngrams
+    n = 4            # number of tokens per n-gram
+
+    def __init__(self, nlp: Language, n: int = None, name: str = None, attr: str = None):
         """Initialize the n-gram component."""
         # register attribute getter on Doc with customizable name; see:
         # https://spacy.io/usage/processing-pipelines#custom-components-best-practices
-        Doc.set_extension(attr, getter=self.get_doc_ngrams)
+        self.attr = attr if attr else self.attr
+        Doc.set_extension(self.attr, getter=self.get_doc_ngrams)
 
-        # store the name that will appear in the pipeline
-        self.name = name if name else "ngrams"
+        # store other properties if provided
+        self.name = name if name else self.name
+        self.n = n if n else self.n
+        logging.info(
+            f"created {self.__class__} as \"{self.name}\" with n={n}, attr=Doc._.{self.attr}")
 
-        # store value for "n", i.e. size of n-grams
-        self.n = n
-        logging.info(f"created {self.__class__} as {self.name} with n={n}")
+    def __del__(self) -> None:
+        """Unregister the n-gram custom extension."""
+        # this is mostly used for tests, to prevent name collisions
+        Doc.remove_extension(self.attr)
 
     def __call__(self, doc: Doc) -> Doc:
         """Return the Doc unmodified."""
@@ -33,4 +42,7 @@ class Ngrams():
 
     def get_doc_ngrams(self, doc: Doc) -> Iterator[Span]:
         """Return an iterator over n-grams in a Doc as Spans."""
+        # if empty doc, nothing should happen
+        if len(doc) == 0:
+            return iter([])
         return (doc[i:i + self.n] for i in range(max(len(doc) - self.n + 1, 1)))

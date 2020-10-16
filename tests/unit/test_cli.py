7b97dd09af078c07863cfd898b540e0ff504aca5
Nick Budak <nbudak@princeton.edu>
2020-10-16 17:37:03 -0400

    Test --min and --max options

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,12 +1,26 @@
 """Tests for the cli module."""
 
+import logging
 import sys
 import tempfile
 from io import StringIO
 from unittest import TestCase, skip
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
-from dphon.cli import run, __doc__ as doc, __version__ as version
+import spacy
+from dphon.cli import __doc__ as doc
+from dphon.cli import __version__ as version
+from dphon.cli import process, run, setup, teardown
+from dphon.index import Index
+from dphon.ngrams import Ngrams
+from spacy.lang.zh import ChineseDefaults
+from spacy.tokens import Doc
+
+# disconnect logging for testing
+logging.disable(logging.CRITICAL)
+
+# turn off default settings for spacy's chinese model
+ChineseDefaults.use_jieba = False
 
 
 class TestCommands(TestCase):
@@ -30,15 +44,28 @@ class TestCommands(TestCase):
 class TestOptions(TestCase):
     """Test the various options available when running."""
 
-    @skip("todo")
+    def setUp(self) -> None:
+        """Set up a spaCy pipeline and mock progressbar for testing."""
+        self.progress = MagicMock()
+        self.nlp = setup()
+
+    def tearDown(self) -> None:
+        """Unregister components to prevent name collisions."""
+        teardown(self.nlp)
+
     def test_min(self) -> None:
         """--min option should limit to results with specified minimum length"""
-        pass
+        args = {"--min": "50", "<path>": ["tests/fixtures/laozi/"]}
+        results = process(self.nlp, self.progress, args)
+        for result in results:
+            self.assertTrue(len(result) >= 50)
 
-    @skip("todo")
     def test_max(self) -> None:
         """--max option should limit to results with specified maximum length"""
-        pass
+        args = {"--max": "4", "<path>": ["tests/fixtures/laozi/"]}
+        results = process(self.nlp, self.progress, args)
+        for result in results:
+            self.assertTrue(len(result) <= 4)
 
     @skip("todo")
     def test_min_and_max(self) -> None:

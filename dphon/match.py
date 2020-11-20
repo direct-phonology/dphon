8168805ac3e06bcd0cc45887561eaeb5a9a63ff1
Nick Budak <nbudak@princeton.edu>
2020-11-20 14:52:17 -0500

    Shift reponsibility out of Match class

--- dphon/match.py
+++ dphon/match.py
@@ -2,16 +2,16 @@
 
 from typing import List, Optional, Tuple
 
-from spacy.tokens import Doc, Span
+from spacy.tokens import Span
 
 
 class NotAlignedError(RuntimeError):
-    """Attempted to access an alignment when none exists."""
+    """Attempted to access a match alignment when none exists."""
     pass
 
 
 class Match():
-    """Matches are immutable pairs of locations in two documents."""
+    """Matches are immutable pairs of sequences in two documents."""
 
     _left: Span
     _right: Span
@@ -19,48 +19,34 @@ class Match():
     _alignment: Optional[Tuple[str, str]] = None
 
     def __init__(self, left: Span, right: Span, alignment: Tuple[List[str], List[str]] = None, score: float = None):
-        """Create a new Match with shallow copies of its two locations."""
+        """Create a new Match with shallow copies of its two sequences."""
         self._left = left.doc[left.start:left.end]
         self._right = right.doc[right.start:right.end]
         self._score = score
-        # store joined string version of aligned text
+        # if an alignment was provided, store it as strings
         if alignment:
-            self._alignment = ("".join(alignment[0]).replace("-", "　"),
-                               "".join(alignment[1]).replace("-", "　"))
+            self._alignment = ("".join(alignment[0]), "".join(alignment[1]))
 
     def __repr__(self) -> str:
-        """Return the match locations as a string."""
-        return f"Match({self._left.doc._.title}[{self._left.start}:{self._left.end}], {self._right.doc._.title}[{self._right.start}:{self._right.end}])"
+        """Return the match sequence locations as a single string."""
+        return f"Match([{self._left.start}:{self._left.end}], [{self._right.start}:{self._right.end}])"
 
     def __str__(self) -> str:
-        """Return the text of the match as a string."""
-        # use aligned text, if it exists
-        if self._alignment:
-            aln_left, aln_right = self._alignment
-            return f"{aln_left} ({self._left.doc._.title})\n{aln_right} ({self._right.doc._.title})"
-        else:
-            return f"{self._left.text} ({self._left.doc._.title})\n{self._right.text} ({self._right.doc._.title})"
+        """Return the texts of the match sequences as a single string."""
+        return f"{self._left.text} :: {self._right.text}"
 
     def __lt__(self, other: object) -> bool:
-        """Order matches by left location, then right. Group by doc title."""
+        """Order matches by left location, then right."""
         if not isinstance(other, Match):
             return False
-        if self._left.doc._.title < other.left.doc._.title:
+        if self._left < other.left:
             return True
-        if self._left.doc._.title == other.left.doc._.title:
-            if self._right.doc._.title < other.right.doc._.title:
-                return True
-            if self._right.doc._.title == other.right.doc._.title:
-                if self._left < other.left:
-                    return True
-                if self._left == other.left:
-                    return self._right < other.right
-                return False
-            return False
+        if self._left == other.left:
+            return self._right < other.right
         return False
 
     def __eq__(self, other: object) -> bool:
-        """Two matches are equal if their docs and locations are identical."""
+        """Two matches are equal if their sequences are identical."""
         if not isinstance(other, Match):
             return False
         if self._left != other.left or self._right != other.right:
@@ -68,7 +54,7 @@ class Match():
         return True
 
     def __len__(self) -> int:
-        """Return the length of the longer of the two locations in the match."""
+        """Return the length of the longer sequence in the match."""
         return max(len(self.left), len(self.right))
 
     @property
@@ -83,7 +69,10 @@ class Match():
 
     @property
     def alignment(self) -> Tuple[str, str]:
-        """Return the two sequences of the match with characters aligned."""
+        """Return the two sequences of the match with characters aligned.
+
+        Raise an error if this function is called when the match has not yet
+        been aligned."""
         if not self._alignment:
             raise NotAlignedError()
         return self._alignment
@@ -95,7 +84,7 @@ class Match():
 
     @property
     def is_norm_eq(self) -> bool:
-        """True if a match's texts are equal after normalization.
+        """True if a match's sequences are equal after normalization.
 
         This comparison ignores whitespace, case, and punctuation, returning
         True if all alphanumeric characters of the left location match all

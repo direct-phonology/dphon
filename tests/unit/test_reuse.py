08bd8b91ad8aef083fa09a9182d326fddce74830
Nick Budak <nbudak@princeton.edu>
2021-01-26 13:44:11 -0500

    Update and test new ReuseGraph (#139)

--- tests/unit/test_reuse.py
+++ tests/unit/test_reuse.py
@@ -1,58 +1,79 @@
 """Tests for the text reuse module."""
 
-from unittest import TestCase
+from unittest import TestCase, skip
 
 import spacy
-from dphon.reuse import Match
+from dphon.reuse import MatchGraph
+from spacy.tokens import Doc
+from dphon.extend import LevenshteinExtender
 
 
-class TestMatch(TestCase):
-    """Test the Match class."""
+class TestMatchGraph(TestCase):
+    """Test the MatchGraph class."""
 
-    maxDiff = None   # don't limit length of diff output for failures
+    maxDiff = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """register spaCy extensions"""
+        Doc.set_extension("title", default="")
+
+    @classmethod
+    def tearDown(cls) -> None:
+        """unregister spaCy extensions to prevent collisions"""
+        Doc.remove_extension("title")
 
     def setUp(self) -> None:
-        """Create example Docs to test with."""
-        self.nlp = spacy.blank("en")
-        self.doc1 = self.nlp.make_doc("a bumblebee under a glass tumbler")
-        self.doc2 = self.nlp.make_doc("an inverted glass tumbler of fireflies")
+        """create a spaCy pipeline and match graph for testing"""
+        self.nlp = spacy.blank(
+            "zh", meta={"tokenizer": {"config": {"use_jieba": False}}})
+        self.G = MatchGraph()
+        # doc1 = self.nlp.make_doc("與朋友交言而有信雖曰未學吾必謂之學矣")
+        # doc2 = self.nlp.make_doc("與朋友交言而有信雖曰已學吾必謂之未也")
+        # doc3 = self.nlp.make_doc("與朋友交言而有信雖未讀書吾亦謂之學矣")
 
-    def test_init(self) -> None:
-        """should make a shallow copy of its provided locations on init"""
-        left = self.doc1[4:6]   # "glass tumbler"
-        right = self.doc2[2:4]  # "glass tumbler"
-        match = Match(left, right)
-        # match has its own copy of the spans
-        self.assertNotEqual(id(left), id(match.left))
-        self.assertNotEqual(id(right), id(match.right))
-        # but its spans point to the same tokens in memory
-        self.assertEqual(id(left[0]), id(match.left[0]))    # "glass"
-        self.assertEqual(id(right[0]), id(match.right[0]))  # "glass"
+    @skip("todo")
+    def test_combine(self) -> None:
+        """combine should combine overlapping matches"""
+        doc1 = self.nlp.make_doc("與朋友交言而有信雖曰未學吾")
+        doc2 = self.nlp.make_doc("與朋友交言而有信雖曰已學吾")
+        doc3 = self.nlp.make_doc("與朋友交言而有信雖未讀書吾")
+        self.G.add_docs([("論語·學而", doc1),
+                         ("藝文類聚·錢", doc2),
+                         ("顏氏家訓·勉學", doc3)])
+        self.G.add_matches([
+            ("論語·學而", "藝文類聚·錢", doc1[0:4], doc2[0:4]),         # 與朋友交
+            ("論語·學而", "藝文類聚·錢", doc1[4:8], doc2[4:8]),         # 言而有信
+            ("論語·學而", "顏氏家訓·勉學", doc1[0:4], doc3[0:4]),       # 與朋友交
+            ("論語·學而", "顏氏家訓·勉學", doc1[4:8], doc3[4:8]),       # 言而有信
+            ("藝文類聚·錢", "顏氏家訓·勉學", doc2[0:4], doc3[0:4]),     # 與朋友交
+            ("藝文類聚·錢", "顏氏家訓·勉學", doc2[4:8], doc3[4:8]),     # 言而有信
+        ])
+        extender = LevenshteinExtender(threshold=0.8, len_limit=50)
+        self.G.extend(extender)
+        self.assertTrue(True)
 
-    def test_lt(self) -> None:
-        """should form a total order by position"""
-        doc1 = self.nlp.make_doc("A B C D A B C D E F G H")
-        doc2 = self.nlp.make_doc("Z Z G H Z Z C D Z Z Z Z")
-        doc3 = self.nlp.make_doc("E F X X A B C D X X X X")
-        m1 = Match(doc1[8:10], doc3[0:2])  # 1:EF :: 3:EF
-        m2 = Match(doc1[0:4], doc3[4:9])  # 1:ABCD :: 3:ABCD
-        m3 = Match(doc1[10:12], doc2[0:2])  # 1:GH :: 2:GH
-        m4 = Match(doc1[4:9], doc3[4:9])  # 1:ABCD :: 3:ABCD
-        m5 = Match(doc1[2:4], doc2[6:8])  # 1:CD :: 2:CD
-        m6 = Match(doc1[7:9], doc2[6:8])  # 1:CD :: 2:CD
-        m7 = Match(doc2[6:8], doc3[6:8])  # 2:CD :: 3:CD
-        m_unsorted = [m1, m2, m3, m4, m5, m6, m7]
-        m_sorted = list(sorted(m_unsorted))
-        self.assertEqual(m_sorted, [m2, m5, m4, m7, m6, m1, m3])
+    @skip("todo")
+    def test_extend(self) -> None:
+        """extend should reduce graph to maximal matches only"""
+        doc1 = self.nlp.make_doc("與朋友交言而有信雖曰未學吾")
+        doc2 = self.nlp.make_doc("與朋友交言而有信雖曰已學吾")
+        doc3 = self.nlp.make_doc("與朋友交言而有信雖未讀書吾")
+        self.G.add_docs([("論語·學而", doc1),
+                         ("藝文類聚·錢", doc2),
+                         ("顏氏家訓·勉學", doc3)])
+        self.G.add_matches([
+            ("論語·學而", "藝文類聚·錢", doc1[0:4], doc2[0:4]),         # 與朋友交
+            ("論語·學而", "藝文類聚·錢", doc1[4:8], doc2[4:8]),         # 言而有信
+            ("論語·學而", "顏氏家訓·勉學", doc1[0:4], doc3[0:4]),       # 與朋友交
+            ("論語·學而", "顏氏家訓·勉學", doc1[4:8], doc3[4:8]),       # 言而有信
+            ("藝文類聚·錢", "顏氏家訓·勉學", doc2[0:4], doc3[0:4]),     # 與朋友交
+            ("藝文類聚·錢", "顏氏家訓·勉學", doc2[4:8], doc3[4:8]),     # 言而有信
+        ])
+        extender = LevenshteinExtender(threshold=0.8, len_limit=50)
+        self.G.extend(extender)
+        self.assertTrue(True)
 
-    def test_eq(self) -> None:
-        """should test equality via position"""
-        left = self.doc1[4:6]
-        right = self.doc2[2:4]
-        # shallow copies of spans should be equal
-        m1 = Match(left, right)
-        m2 = Match(left, right)
-        self.assertEqual(m1, m2)
-        # pre-copied spans should also be equal
-        m3 = Match(self.doc1[4:6], self.doc2[2:4])
-        self.assertEqual(m1, m3)
+    @skip("todo")
+    def test_filter(self) -> None:
+        return

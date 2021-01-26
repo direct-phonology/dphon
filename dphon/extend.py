8ca2d93d2a0ba827c6f4a5c4e2b92366c3ffc216
Nick Budak <nbudak@princeton.edu>
2021-01-26 13:41:49 -0500

    Refactor extension to use graph strategy
    - Refactor Extender classes
    - Move extend_matches function to Extender module
    
    See #139

--- dphon/extend.py
+++ dphon/extend.py
@@ -1,12 +1,13 @@
 """Abstract base class and implementations for extending Matches."""
 
 from abc import ABC, abstractmethod
+from typing import List
 
 import Levenshtein as Lev
 from spacy.tokens import Span
 
+from dphon.match import Match
 from dphon.phonemes import OOV_PHONEMES
-from dphon.reuse import Match
 
 
 class Extender(ABC):
@@ -46,46 +47,45 @@ class StringDistanceExtender(Extender):
     def _extend_fwd(self, match: Match) -> Match:
         """Return a copy of a match extended in the forward direction."""
         trail = 0
-        score = self._score(match.left, match.right)
-        left = match.left.doc[match.left.start:match.left.end]
-        right = match.right.doc[match.right.start:match.right.end]
-        left_len, right_len = len(match.left.doc), len(match.right.doc)
+        score = self._score(match.utxt, match.vtxt)
+        utxt = match.utxt.doc[match.utxt.start:match.utxt.end]
+        vtxt = match.vtxt.doc[match.vtxt.start:match.vtxt.end]
+        ulen, vlen = len(match.utxt.doc), len(match.vtxt.doc)
 
         # extend while score is above threshold and we aren't at the end
-        while score >= self.threshold and \
-                left.end < left_len and right.end < right_len:
-            left = left.doc[left.start:left.end + 1]
-            right = right.doc[right.start:right.end + 1]
+        while score >= self.threshold and utxt.end < ulen and vtxt.end < vlen:
+            utxt = utxt.doc[utxt.start:utxt.end + 1]
+            vtxt = vtxt.doc[vtxt.start:vtxt.end + 1]
 
             # track the last score increase and how far we've gone past it
-            new_score = self._score(left, right)
+            new_score = self._score(utxt, vtxt)
             trail = trail + 1 if new_score < score else 0
             score = new_score
 
         # return match trimmed back to last increasing score
-        return Match(left.doc[left.start:left.end - trail],
-                     right.doc[right.start:right.end - trail])
+        return Match(match.u, match.v, utxt.doc[utxt.start:utxt.end - trail],
+                     vtxt.doc[vtxt.start:vtxt.end - trail])
 
     def _extend_rev(self, match: Match) -> Match:
         """Return a copy of a match extended in the reverse direction."""
         trail = 0
-        score = self._score(match.left, match.right, rev=True)
-        left = match.left.doc[match.left.start:match.left.end]
-        right = match.right.doc[match.right.start:match.right.end]
+        score = self._score(match.utxt, match.vtxt, rev=True)
+        utxt = match.utxt.doc[match.utxt.start:match.utxt.end]
+        vtxt = match.vtxt.doc[match.vtxt.start:match.vtxt.end]
 
         # extend while score is above threshold and we aren't at the start
-        while score >= self.threshold and left.start > 0 and right.start > 0:
-            left = left.doc[(left.start-1):left.end]
-            right = right.doc[(right.start-1):right.end]
+        while score >= self.threshold and utxt.start > 0 and vtxt.start > 0:
+            utxt = utxt.doc[(utxt.start-1):utxt.end]
+            vtxt = vtxt.doc[(vtxt.start-1):vtxt.end]
 
             # track the last score increase and how far we've gone past it
-            new_score = self._score(left, right, rev=True)
+            new_score = self._score(utxt, vtxt, rev=True)
             trail = trail + 1 if new_score < score else 0
             score = new_score
 
         # return match trimmed back to last increasing score
-        return Match(left.doc[left.start + trail:left.end],
-                     right.doc[right.start + trail:right.end])
+        return Match(match.u, match.v, utxt.doc[utxt.start + trail:utxt.end],
+                     vtxt.doc[vtxt.start + trail:vtxt.end])
 
     def __call__(self, match: Match) -> Match:
         """Extend a match using Levenshtein ratio comparison.
@@ -95,29 +95,27 @@ class StringDistanceExtender(Extender):
 
         ex_fwd = self._extend_fwd(match)
         ex_rev = self._extend_rev(match)
-
-        # use the extended bounds to update the match and score it
-        match.left = match.left.doc[ex_rev.left.start:ex_fwd.left.end]
-        match.right = match.right.doc[ex_rev.right.start:ex_fwd.right.end]
-        match.score = self._score(match.left, match.right)
-        return match
+        utxt = match.utxt.doc[ex_rev.utxt.start:ex_fwd.utxt.end]
+        vtxt = match.vtxt.doc[ex_rev.vtxt.start:ex_fwd.vtxt.end]
+        score = self._score(utxt, vtxt)
+        return Match(match.u, match.v, utxt, vtxt, score)
 
 
 class LevenshteinExtender(StringDistanceExtender):
     """Add tokens to sequences while Levenshtein ratio is above threshold."""
 
-    def _score(self, left: Span, right: Span, rev: bool = False) -> float:
+    def _score(self, utxt: Span, vtxt: Span, rev: bool = False) -> float:
         """Compute the Levenshtein ratio of the match sequences."""
 
         if rev:
             return Lev.ratio(
-                left.text[:self.len_limit],
-                right.text[:self.len_limit]
+                utxt.text[:self.len_limit],
+                vtxt.text[:self.len_limit]
             )
         else:
             return Lev.ratio(
-                left.text[-self.len_limit:],
-                right.text[-self.len_limit:]
+                utxt.text[-self.len_limit:],
+                vtxt.text[-self.len_limit:]
             )
 
 
@@ -125,20 +123,59 @@ class LevenshteinPhoneticExtender(StringDistanceExtender):
     """Add tokens to sequences while Levenshtein ratio of phonemes is above
     threshold."""
 
-    def _score(self, left: Span, right: Span, rev: bool = False) -> float:
+    def _score(self, utxt: Span, vtxt: Span, rev: bool = False) -> float:
         """Compute the Levenshtein ratio of the match sequence phonemes."""
 
         # if we encounter any OOV tokens, count it as a mismatch
-        if OOV_PHONEMES in left._.phonemes or \
-           OOV_PHONEMES in right._.phonemes:
+        if OOV_PHONEMES in utxt._.phonemes or OOV_PHONEMES in vtxt._.phonemes:
             return -1
 
         # otherwise score based on phonemes
-        text1 = "".join(left._.phonemes)
-        text2 = "".join(right._.phonemes)
+        text1 = "".join(utxt._.phonemes)
+        text2 = "".join(vtxt._.phonemes)
 
         # score in the provided direction
         if rev:
             return Lev.ratio(text1[:self.len_limit], text2[:self.len_limit])
         else:
             return Lev.ratio(text1[-self.len_limit:], text2[-self.len_limit:])
+
+
+def extend_matches(matches: List[Match], extend: Extender) -> List[Match]:
+    """Extend a list of matches using a provided Extender instance."""
+    # track working matches in a queue; store finished ones separately
+    working: List[Match] = []
+    done: List[Match] = []
+
+    # track unprocessed matches in a queue; sort then reverse so we can pop()
+    # matches from the queue until it's empty, starting at the front
+    todo = list(reversed(sorted(matches)))
+    while todo:
+        current = todo.pop()
+
+        # if we're not working on any matches yet, or if the current match is
+        # outside working point in U, clear and reset the working area
+        if not working or current.utxt.start >= working[0].utxt.end:
+            done += working
+            working = [extend(current)]
+
+        # if we overlap in U, check to see if we also overlap in V for any
+        # matches we're working on
+        else:
+            skip = False
+            for match in working:
+                # if match is fully internal to one in the working area, skip
+                # it, as we no longer need it
+                if current.vtxt.start >= match.vtxt.start and \
+                   current.vtxt.start <= match.vtxt.end and \
+                   current.vtxt.end <= match.vtxt.end:
+                    skip = True
+                    break
+            # if match hits new location in V, extend it and add it to
+            # the working area
+            if not skip:
+                working.append(extend(current))
+
+    # finish any remaining work and return extended matches
+    done += working
+    return done

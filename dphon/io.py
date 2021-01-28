4b17f4fc06fbda8364d25244a4f071681825d2ec
Nick Budak <nbudak@princeton.edu>
2021-01-28 13:56:50 -0500

    Add CorpusLoader and implementations
    - Extracts load_text into PlaintextCorpusLoader class
    - Creates ABC CorpusLoader class
    - Creates JsonlinesCorpusLoader class for loading .jsonl files
    
    See #42

---
+++ dphon/io.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Classes for loading document corpora and passing them to an NLP pipeline."""
+
+import logging
+import jsonlines
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from glob import glob
+from pathlib import Path
+from typing import Any, Dict, Iterable, Tuple
+
+# Type for a doc ready to be indexed by spaCy's `nlp.pipe(as_tuples=True)`:
+# (content, metadata) where content is a string and metadata is a dict
+DocInfo_T = Tuple[str, Dict[str, Any]]
+
+
+class CorpusLoader(ABC):
+    """Abstract base class; implements loading of document corpora."""
+
+    @abstractmethod
+    def __call__(self, paths: Iterable[str]) -> Iterable[DocInfo_T]:
+        """Load valid files from all paths, returning contents and metadata.
+
+        Output is a single tuple of (contents, metadata) where "contents" is the
+        contents of the file as a string and "metadata" is an arbitrary dict.
+
+        One tuple per doc should be returned for consumption by spaCy's 
+        `nlp.pipe(as_tuples=True)`.
+        """
+        raise NotImplementedError
+
+    def _check(self, paths: Iterable[str]) -> Dict[Path, Any]:
+        """Check each of the provided paths and output a list of valid files."""
+
+        # track how many valid files we found, and store valid ones with their
+        # metadata for loading
+        total = 0
+        files = {}
+
+        # try to create a `pathlib.Path` from each expanded path; store it
+        # if we succeed so that we can later open the file using it
+        for path in paths:
+            for file in map(Path, glob(path)):
+                if file.is_file():
+                    size = file.stat().st_size
+                    files[file] = {"size": size, "id": file.stem}
+                    total += 1
+                    logging.debug(f"found {file}, size={size}B")
+                else:
+                    logging.warn(f"path {file} isn't a file")
+
+        # if no valid files were found, warn the user. otherwise report the
+        # total number of files
+        if not total:
+            logging.warning("no valid files found")
+        else:
+            logging.debug(f"found {total} total files")
+        return files
+
+
+class PlaintextCorpusLoader(CorpusLoader):
+    """Loads documents stored as one or more .txt files."""
+
+    def __call__(self, paths: Iterable[str]) -> Iterable[DocInfo_T]:
+        """Load valid files and metadata and yield in order of size, desc.
+
+        All provided paths will be searched, and globs will be expanded, e.g.
+        /my/home/dir/*.txt will yield all plaintext files in /my/home/dir/.
+
+        File metadata consists of the file's name, minus extension (the "stem")
+        and the file size on disk in bytes.
+
+        Args:
+            paths: Iterable of .txt file paths to load.
+
+        Yields:
+            A tuple of (contents, metadata) for each valid document found.
+        """
+
+        # sort files by size, largest first, to speed up processing by spaCy
+        files = self._check(paths)
+        files_by_size = OrderedDict(sorted(files.items(),
+                                           key=lambda f: f[1]["size"],
+                                           reverse=True))
+
+        # open each file and yield contents with metadata as DocInfo_T
+        for file, meta in files_by_size.items():
+            with file.open(encoding="utf8") as contents:
+                logging.debug(
+                    f"loaded doc \"{meta['id']}\" from {file}")
+                yield contents.read(), meta
+
+
+class JsonLinesCorpusLoader(CorpusLoader):
+    """Loads documents stored as lines in one or more .jsonl files."""
+
+    def __call__(self, paths: Iterable[str]) -> Iterable[DocInfo_T]:
+        """Parse .jsonl files and yield document text and metadata.
+
+        All provided paths will be searched, and globs will be expanded, e.g.
+        /my/home/dir/*.jsonl will yield all jsonlines files in /my/home/dir/.
+
+        Each .jsonl file is assumed to consist of lines where each line is a
+        valid JSON object. The only required properties are "id", a unique name
+        for the document, and "text", the text of the document itself. All
+        other properties will be passed through to spaCy.
+
+        Args:
+            paths: Iterable of .jsonl file paths to load.
+
+        Yields:
+            A tuple of (contents, metadata) for each valid document found.
+        """
+
+        # open each file and yield each line, with all properties except "text"
+        # being passed as second element in tuple
+        files = self._check(paths)
+        for file in files.keys():
+            with jsonlines.open(file) as reader:
+                for doc in reader:
+                    meta = {k: v for k, v in doc.items() if k != "text"}
+                    logging.debug(
+                        f"loaded doc \"{doc['id']}\" from {file}")
+                    yield doc["text"], meta

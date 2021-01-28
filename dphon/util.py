9e744c504e43296f57723be77723cdcf4b16556b
Nick Budak <nbudak@princeton.edu>
2021-01-28 13:56:24 -0500

    Remove unused util functions

--- dphon/util.py
+++ dphon/util.py
@@ -1,17 +1,8 @@
-"""
-Utility functions
-"""
-
-import logging
-from itertools import chain, groupby
-from operator import attrgetter
-from pathlib import Path
-from typing import Any, Deque, Dict, Iterable, List, Tuple
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Utility functions."""
 
 from rich.progress import BarColumn, Progress
-
-from dphon.extend import Extender
-from dphon.match import Match
 
 progress = Progress(
     "{task.elapsed:.0f}s",
@@ -22,14 +13,3 @@ progress = Progress(
     transient=True
 )
 
-def get_texts(directory: Path) -> List[Tuple[str, Dict[str, Any]]]:
-    # load all texts and format with context
-    texts = []
-    for file in directory.glob("**/*.txt"):
-        with file.open(encoding="utf8") as contents:
-            text = contents.read()
-            texts.append((text, {"title": file.stem, "len": len(text)}))
-    logging.info(f"loaded {len(texts)} texts from {directory}")
-    # return in order with largest texts first, to speed up processing
-    # confirmed this is slightly faster on my machine 2020-09-04
-    return sorted(texts, key=lambda t: t[1]["len"], reverse=True)

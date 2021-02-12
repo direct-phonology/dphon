c2a6586ec18e812eb95e5258cb13cf6f1b397be6
Nick Budak <nbudak@princeton.edu>
2021-02-12 15:33:33 -0500

    Make Ngrams component spacy 3.0 compliant

--- dphon/ngrams.py
+++ dphon/ngrams.py
@@ -8,37 +8,22 @@ from typing import Iterator
 from spacy.language import Language
 from spacy.tokens import Doc, Span
 
-# register the pipeline component factory; see:
-# https://spacy.io/usage/processing-pipelines#custom-components-factories
-Language.factories["ngrams"] = lambda nlp, **cfg: Ngrams(nlp, **cfg)
-
 
 class Ngrams():
     """A spaCy pipeline component for generating Token n-grams from Docs."""
 
-    name = "ngrams"  # will appear in spaCy pipeline
-    attr = "ngrams"  # name for getter for ngrams, e.g. doc._.ngrams
-    n: int           # number of tokens per n-gram (order)
+    n: int  # number of tokens per n-gram (order)
 
-    def __init__(self, nlp: Language, n: int, name: str = None, attr: str = None):
+    def __init__(self, nlp: Language, n: int):
         """Initialize the n-gram component."""
-        # register attribute getter on Doc with customizable name; see:
-        # https://spacy.io/usage/processing-pipelines#custom-components-best-practices
-        self.attr = attr if attr else self.attr
-        Doc.set_extension(self.attr, getter=self.get_doc_ngrams)
-
-        # store other properties
         self.n = n
-        self.name = name if name else self.name
-        logging.info(f"using {self.__class__}\" with n={self.n}, name={self.name}")
+        if not Doc.has_extension("ngrams"):
+            Doc.set_extension("ngrams", getter=self.get_doc_ngrams)
+        logging.info(f"using {self.__class__}\" with n={self.n}")
 
     def __call__(self, doc: Doc) -> Doc:
         """Return the Doc unmodified."""
         return doc
-
-    def teardown(self) -> None:
-        """Unregister the attribute to prevent collisions."""
-        Doc.remove_extension(self.attr)
 
     def get_doc_ngrams(self, doc: Doc) -> Iterator[Span]:
         """Return an iterator over n-grams in a Doc as Spans."""
@@ -46,3 +31,8 @@ class Ngrams():
         if len(doc) == 0:
             return iter([])
         return (doc[i:i + self.n] for i in range(max(len(doc) - self.n + 1, 1)))
+
+
+@Language.factory("ngrams")
+def create_ngrams(nlp: Language, name: str, n: int) -> Ngrams:
+    return Ngrams(nlp, n)
--- tests/unit/test_ngrams.py
+++ tests/unit/test_ngrams.py
@@ -1,61 +1,39 @@
 """Tests for the n-grams module."""
 
+from dphon.ngrams import Ngrams
 from unittest import TestCase
 
 import spacy
 from spacy.tokens import Doc
-
-from dphon.ngrams import Ngrams
 
 
 class TestNgrams(TestCase):
     """Test the n-grams spaCy pipeline component."""
 
     def setUp(self) -> None:
-        """Create a blank spaCy pipeline to use in tests."""
+        """create blank spaCy language for testing"""
         self.nlp = spacy.blank("en")
 
-    def tearDown(self) -> None:
-        """Unregister the component to prevent name collisions."""
-        if hasattr(self, "ng"):
-            self.ng.teardown()
-
     def test_defaults(self) -> None:
-        """should populate name and attr by default and store n"""
-        self.ng = Ngrams(self.nlp, n=4)
-        self.nlp.add_pipe(self.ng)
-        self.assertEqual(self.ng.n, 4)
+        """should store value for n and set extensions"""
+        ngrams = Ngrams(self.nlp, n=4)
+        self.assertEqual(ngrams.n, 4)
         self.assertTrue(Doc.has_extension("ngrams"))
-        self.assertTrue(self.nlp.has_pipe("ngrams"))
-
-    def test_custom_name(self) -> None:
-        """should accept a custom name for pipeline component"""
-        self.ng = Ngrams(self.nlp, name="my_ngrams", n=4)
-        self.nlp.add_pipe(self.ng)
-        self.assertTrue(self.nlp.has_pipe("my_ngrams"))
-
-    def test_custom_attr(self) -> None:
-        """should accept a custom attribute name for accessing ngrams"""
-        self.ng = Ngrams(self.nlp, attr="my_ngrams", n=4)
-        self.nlp.add_pipe(self.ng)
-        self.assertTrue(Doc.has_extension("my_ngrams"))
 
     def test_unigrams(self) -> None:
         """should create single-token n-grams successfully"""
-        self.ng = Ngrams(self.nlp, n=1)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("It was a dark and stormy night")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=1)
+        doc = self.nlp("It was a dark and stormy night")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, [
             "It", "was", "a", "dark", "and", "stormy", "night"
         ])
 
     def test_bigrams(self) -> None:
         """should create 2-token n-grams successfully"""
-        self.ng = Ngrams(self.nlp, n=2)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("It was a dark and stormy night")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=2)
+        doc = self.nlp("It was a dark and stormy night")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, [
             "It was", "was a", "a dark", "dark and", "and stormy",
             "stormy night"
@@ -63,10 +41,9 @@ class TestNgrams(TestCase):
 
     def test_trigrams(self) -> None:
         """should create 3-token n-grams successfully"""
-        self.ng = Ngrams(self.nlp, n=3)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("It was a dark and stormy night")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=3)
+        doc = self.nlp("It was a dark and stormy night")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, [
             "It was a", "was a dark", "a dark and", "dark and stormy",
             "and stormy night"
@@ -74,24 +51,21 @@ class TestNgrams(TestCase):
 
     def test_empty_doc(self) -> None:
         """should handle an empty doc"""
-        self.ng = Ngrams(self.nlp, n=3)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=3)
+        doc = self.nlp("")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, [])
 
     def test_one_token_doc(self) -> None:
         """should handle a doc with one token"""
-        self.ng = Ngrams(self.nlp, n=3)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("Nope")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=3)
+        doc = self.nlp("Nope")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, ["Nope"])
 
     def test_n_larger_than_doc(self) -> None:
         """should handle a value for n larger than the doc itself"""
-        self.ng = Ngrams(self.nlp, n=5)
-        self.nlp.add_pipe(self.ng)
-        doc = self.nlp.make_doc("No way")
-        results = [str(ngram) for ngram in doc._.ngrams]
+        ngrams = Ngrams(self.nlp, n=5)
+        doc = self.nlp("No way")
+        results = [str(ngram) for ngram in ngrams.get_doc_ngrams(doc)]
         self.assertEqual(results, ["No way"])

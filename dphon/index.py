c46f359c509da02e30e58478e6830fc7d6d12c19
Nick Budak <nbudak@princeton.edu>
2021-02-12 15:33:22 -0500

    Make Index components spacy 3.0 compliant & refactor

--- dphon/index.py
+++ dphon/index.py
@@ -1,99 +1,131 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""SpaCy pipeline components for building indices of arbitrary Token data."""
+"""SpaCy pipeline components for building indices of arbitrary document data."""
 
 import logging
-from itertools import filterfalse
-from typing import Any, Callable, Iterator, Iterable, List, Tuple, TypeVar, Optional
+from abc import ABC, abstractmethod
+from typing import Callable, Hashable, Iterable, Iterator, List, Tuple, TypeVar, Generic
 
 from spacy.language import Language
+from spacy.tokens import Doc, Span
 from spacy.lookups import Table
-from spacy.tokens import Doc, Span, Token
 
-# register the pipeline component factory; see:
-# https://spacy.io/usage/processing-pipelines#custom-components-factories
-Language.factories["index"] = lambda nlp, **cfg: Index(nlp, **cfg)
-
-KT = TypeVar("KT")              # type for keys stored in the index
-VT = TypeVar("VT")              # type for values stored in the index
-Entry = Tuple[KT, List[VT]]     # type for an index entry (all values at key)
-
-# called on a Doc; returns the list of values from the doc we want to index
-ValFn = Callable[[Doc], Iterable[VT]]
-
-# called on a value; if False the value will not be indexed
-FilterFn = Callable[[VT], bool]
-
-# called on a value to index; returns the key to index at (i.e. hash)
-KeyFn = Callable[[VT], KT]
+K = TypeVar("K")                    # type for keys stored in the index
+V = TypeVar("V")                    # type for values stored in the index
 
 
-class Index():
+class Index(ABC, Generic[K, V]):
     """A spaCy pipeline component that indexes arbitrary items via callables."""
 
-    name = "index"  # will appear in spaCy pipeline
+    def __init__(self, nlp: Language):
+        """Initialize the index component."""
+        logging.info(f"using {self.__class__}")
 
-    def __init__(self, nlp: Language, key_fn: Optional[KeyFn] = None,
-                 val_fn: Optional[ValFn] = None, filter_fn: Optional[FilterFn] = None,
-                 name: str = None):
-        """Initialize the index component.
+    @abstractmethod
+    def __call__(self, doc: Doc) -> Doc:
+        """Index a single spaCy Doc. Should not mutate the Doc."""
+        return doc
 
-        - By default, an index will store lists of Tokens keyed on their text.
-        - Pass val_fn to control what values are extracted from a Doc.
-        - Pass key_fn to control what key values are indexed at.
-        - Pass filter_fn to control which values are indexed."""
-        # store name, callables used to index values, and total values (size)
-        self.name = name if name else self.name
+    @abstractmethod
+    def __contains__(self, key: K) -> bool:
+        """Check if a key is in the index."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, key: K) -> Iterable[V]:
+        """Return all values indexed at a given key."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[Tuple[K, List[V]]]:
+        """Return a (k, v) iterator over all entries in the index."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def filter(self, fn: Callable[[Tuple[K, List[V]]], bool]) -> Iterator[Tuple[K, List[V]]]:
+        """Return a (k, v) iterator over all entries which match a predicate."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """Get the total number of keys in the index."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def size(self) -> int:
+        """Get the total number of values in the index."""
+        raise NotImplementedError
+
+
+class LookupsIndex(Index[Hashable, V], Generic[V]):
+
+    _table: Table       # uses spaCy's lookup tables (bloom filtered dict)
+    _size: int          # tracker for total number of values in index
+
+    def __init__(self, nlp: Language) -> None:
+        self._table = nlp.vocab.lookups.add_table("index")
         self._size = 0
-
-        # by default, extracts tokens from docs and indexes them via their text
-        self.val_fn: ValFn = val_fn if val_fn else lambda doc: doc
-        self.key_fn: KeyFn = key_fn if key_fn else lambda token: token.text
-        self.filter_fn: FilterFn = filter_fn if filter_fn else lambda token: True
-
-        # initialize the index
-        self.table = nlp.vocab.lookups.add_table(self.name)
-        logging.info(f"using {self.__class__} with name={self.name}")
+        super().__init__(nlp)
 
     def __call__(self, doc: Doc) -> Doc:
-        """Extract values from a doc and index those that pass the filter.
-
-        - Values are extracted from the doc using val_fn(doc).
-        - Values that pass the predicate filter_fn(value) are indexed.
-        - Values will be indexed using the output of key_fn(value).
-        - The Doc is returned unmodified."""
-
-        for val in self.val_fn(doc):
-            if self.filter_fn(val):
-                key = self.key_fn(val)
-                try:
-                    self.table.get(key).append(val)
-                except AttributeError:
-                    self.table.set(key, [val])
-                self._size += 1
-        return doc
+        """Extract values from a doc with _get_vals and index with _get_key."""
+        for val in self._get_vals(doc):
+            key = self._get_key(val)
+            try:
+                self._table.get(key).append(val)
+            except AttributeError:
+                self._table.set(key, [val])
+            self._size += 1
+        return super().__call__(doc)
 
     def __len__(self) -> int:
         """Get the total number of keys in the index."""
-        return len(self.table)
+        return len(self._table)
 
-    def __contains__(self, key: KT) -> bool:
+    def __contains__(self, key: K) -> bool:
         """Check if a key is in the index."""
-        return key in self.table
+        return key in self._table
 
-    def __getitem__(self, key: KT) -> List[VT]:
+    def __getitem__(self, key: K) -> List[V]:
         """Return a list of all values indexed at a given key."""
-        return self.table.get(key)
+        return self._table.get(key)
 
-    def __iter__(self) -> Iterator[Entry]:
+    def __iter__(self) -> Iterator[Tuple[K, List[V]]]:
         """Return a (k, v) iterator over all entries in the index."""
-        return self.filter(lambda entry: True)
+        return (entry for entry in self._table.items())
 
-    def filter(self, fn: Callable[[Entry], bool]) -> Iterator[Entry]:
+    @abstractmethod
+    def _get_vals(self, doc: Doc) -> Iterable[V]:
+        """Get all the values to be indexed from a Doc."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_key(self, val: V) -> Hashable:
+        """Get the key to index a particular value."""
+        raise NotImplementedError
+
+    def filter(self, fn: Callable[[Tuple[K, List[V]]], bool]) -> Iterator[Tuple[K, List[V]]]:
         """Return a (k, v) iterator over all entries which match a predicate."""
-        return (entry for entry in self.table.items() if fn(entry))
+        return (entry for entry in iter(self) if fn(entry))
 
     @property
     def size(self) -> int:
         """Get the total number of values in the index."""
         return self._size
+
+
+class NgramPhonemesLookupsIndex(LookupsIndex[Span]):
+
+    def _get_vals(self, doc: Doc) -> Iterator[Span]:
+        """Iterator over alphabetic ngrams in the doc."""
+        return (ngram for ngram in doc._.ngrams if ngram.text.isalpha)
+
+    def _get_key(self, val: Span) -> str:
+        """All phonetic content of an ngram as a string."""
+        return "".join(val._.phonemes)
+
+
+@Language.factory("ngram_phonemes_index")
+def create_ngram_phonemes_lookup_index(nlp: Language, name: str) -> NgramPhonemesLookupsIndex:
+    return NgramPhonemesLookupsIndex(nlp)
--- tests/unit/test_index.py
+++ tests/unit/test_index.py
@@ -2,103 +2,91 @@
 
 import io
 import logging
-from itertools import islice
 from unittest import TestCase
+from typing import Iterator
 
 import spacy
+from spacy import Language
+from spacy.tokens import Doc, Token
 from dphon.console import err_console
-from dphon.index import Index
+from dphon.index import LookupsIndex, NgramPhonemesLookupsIndex
 
 # disconnect logging and capture stderr output for testing
 logging.disable(logging.CRITICAL)
 err_console.file = io.StringIO()
 
 
-class TestIndex(TestCase):
+class TokenTextLookupsIndex(LookupsIndex[Token]):
+    """A LookupsIndex subclass for testing; indexes token texts."""
+
+    def _get_vals(self, doc: Doc) -> Iterator[Token]:
+        return (token for token in doc)
+
+    def _get_key(self, val: Token) -> str:
+        return val.text
+
+
+class TestLookupsIndex(TestCase):
+    """Test the generic LookupsIndex via a subclass."""
+
+    def setUp(self) -> None:
+        """Create a blank spaCy pipeline, index, and doc for testing."""
+        self.nlp = spacy.blank("en")
+        self.idx = TokenTextLookupsIndex(self.nlp)
+        self.doc = self.nlp("To be or not to be")
+        self.idx(self.doc)
+
+    def test_init(self) -> None:
+        """should create lookups table"""
+        self.assertTrue(self.nlp.vocab.lookups.has_table("index"))
+
+    def test_len(self) -> None:
+        """should store total number of keys in index"""
+        # 5 unique token texts; "be" occurs twice
+        self.assertEqual(len(self.idx), 5)
+
+    def test_size(self) -> None:
+        """should store total number of values in index"""
+        # 6 total tokens stored in index
+        self.assertEqual(self.idx.size, 6)
+
+    def test_getitem(self) -> None:
+        """should retrieve all values at a key"""
+        # should index both locations of "be" at same key
+        self.assertEqual(self.idx["be"], [self.doc[1], self.doc[5]])
+
+    def test_contains(self) -> None:
+        """should report if a key is in the index"""
+        # "be" is in the index; "missing" is not
+        self.assertTrue("be" in self.idx)
+        self.assertFalse("missing" in self.idx)
+
+    def test_iter(self) -> None:
+        """should iterate through entries in index"""
+        # when iterating, token texts will be Lexeme objects
+        entries = list(iter(self.idx))
+        self.assertEqual(entries[0], (self.nlp.vocab["To"], [self.doc[0]]))
+        self.assertEqual(entries[1], (self.nlp.vocab["be"], [
+                         self.doc[1], self.doc[5]]))
+
+    def test_filter(self) -> None:
+        """should iterate through entries in index that match predicate"""
+        # test filtering to only unique tokens; "be" occurs more than once
+        unique = list(self.idx.filter(lambda entry: len(entry[1]) == 1))
+        self.assertEqual(unique[0], (self.nlp.vocab["To"], [self.doc[0]]))
+        unique_texts = [self.nlp.vocab[entry[0]] for entry in unique]
+        self.assertTrue("be" not in unique_texts)
+
+
+class TestNgramPhonemesLookupsIndex(TestCase):
     """Test the index spaCy pipeline component."""
 
     def setUp(self) -> None:
-        """Create a blank spaCy pipeline and doc for testing."""
+        """Create a blank spaCy pipeline, index, and doc for testing."""
         self.nlp = spacy.blank("en")
-        self.doc = self.nlp.make_doc("To be or not to be")
+        self.idx = NgramPhonemesLookupsIndex(self.nlp)
+        self.doc = self.nlp("To be or not to be")
 
-    def test_defaults(self) -> None:
-        """should use name `index` and create lookup table by default"""
-        idx = Index(self.nlp)
-        self.nlp.add_pipe(idx)
-        self.assertEqual(idx.name, "index")
-        self.assertTrue(self.nlp.has_pipe("index"))
-        self.assertTrue(self.nlp.vocab.lookups.has_table("index"))
-        
-    def test_val_fn(self) -> None:
-        """can get values to index via a provided callable"""
-        # example: only index the first three tokens from each doc
-        first_three_tokens = lambda doc: islice(doc, 3)
-        idx = Index(self.nlp, val_fn=first_three_tokens)
-        idx(self.doc)
-        self.assertEqual(idx.size, 3) # only indexed 3 tokens total
-        self.assertFalse("not" in idx) # "not" wasn't indexed
-
-    def test_filter_fn(self) -> None:
-        """can skip indexing values if they fail a provided predicate"""
-        # example: index tokens only if they are lowercase
-        all_lowercase = lambda token: token.is_lower
-        idx = Index(self.nlp, filter_fn=all_lowercase)
-        idx(self.doc)
-        self.assertEqual(idx.size, 5) # indexed all but one token
-        self.assertFalse("To" in idx) # "To" wasn't indexed
-
-    def test_key_fn(self) -> None:
-        """can index values at a key generated via a provided callable"""
-        # example: index tokens by their character position
-        token_position = lambda token: token.idx
-        idx = Index(self.nlp, key_fn=token_position)
-        idx(self.doc)
-        self.assertEqual(idx[0], [self.doc[0]]) # "To" is at beginning
-        self.assertEqual(idx[3], [self.doc[1]]) # "be" occurs in two places
-        self.assertEqual(idx[16], [self.doc[5]])
-
-    def test_len(self) -> None:
-        """should get the total number of keys in the index"""
-        idx = Index(self.nlp)
-        idx(self.doc)
-        self.assertEqual(len(idx), 5) # 5 unique token texts
-
-    def test_size(self) -> None:
-        """should get the total number of values in the index"""
-        idx = Index(self.nlp)
-        idx(self.doc)
-        self.assertEqual(idx.size, 6) # 6 total tokens
-
-    def test_getitem(self) -> None:
-        """should access values by key"""
-        idx = Index(self.nlp)
-        idx(self.doc)
-        self.assertEqual(idx["be"], [self.doc[1], self.doc[5]]) # two tokens with "be"
-        self.assertEqual(idx["or"], [self.doc[2]]) # one token with "or"
-
-    def test_contains(self) -> None:
-        """should check if a key is present"""
-        idx = Index(self.nlp)
-        idx(self.doc)
-        self.assertTrue("be" in idx)
-        self.assertFalse("tuna" in idx)
-
-    def test_iter(self) -> None:
-        """should iterate over its key/value pairs"""
-        idx = Index(self.nlp)
-        idx(self.doc)
-        entries = list(iter(idx))
-        # Note that token texts are technically Lexemes so we need to convert
-        self.assertEqual(entries[0], (self.nlp.vocab["To"], [self.doc[0]]))
-        self.assertEqual(entries[1], (self.nlp.vocab["be"], [self.doc[1], self.doc[5]]))
-        self.assertEqual(entries[2], (self.nlp.vocab["or"], [self.doc[2]]))
-
-    def test_filter(self) -> None:
-        """should iterate over a subset of its key/value pairs"""
-        # example: get all the entries with at least two tokens
-        idx = Index(self.nlp)
-        idx(self.doc)
-        entries = list(idx.filter(lambda entry: len(entry[1]) > 1))
-        self.assertEqual(len(entries), 1) # "be" occurs twice
-        self.assertEqual(entries[0][0], self.nlp.vocab["be"])
+    def test_call(self) -> None:
+        """foo"""
+        pass
